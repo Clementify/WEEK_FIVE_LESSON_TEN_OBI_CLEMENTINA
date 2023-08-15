1a40814454bdc1a05c46fe6ee91145a9514c95e3
clementify <obiclementina92@gmail.com>
2023-08-15 15:55:55 +0100

    final commit

--- tasks/task_one.js
+++ tasks/task_one.js
@@ -1,2 +1,17 @@
 
+// Task 1: Create a class of car with the following fields: numSeats, numbHeadLamps, fuelTankMaxLitre, numbExhaustPipe, convertible(boolean), numbOfDoors, numbOfEnginePlug(default to 4). Instantiate the class to create different types of cars using your car class(at least for).
+
 // Write something
+class Car {
+    constructor(numbSeats,numHl,FTML,nEMP,car,doors,numEP =4){
+        this.numberOfSeats = numbSeats;
+        this.numberOfHeadlamps = numHl;
+        this.fuelTankManagement = FTML;
+        this.nEMP = nEMP;
+        this.car = car;
+        this.doors = doors;
+        this.numEP = numEP
+    }
+}
+const Toyota = new Car (54,3, 'max', 'map', 'spider', 'four')
+console.log(Toyota)
